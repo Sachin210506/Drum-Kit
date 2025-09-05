0b557ca04cb409fd53422611dc070c5a75aefcae
Sachin Rohra <sachinrohra05@gmail.com>
2025-09-05 18:15:32 +0530

    Added required Files

---
+++ index.js
@@ -0,0 +1,53 @@
+var n=document.querySelectorAll(".drum").length;
+;
+for(var i=0;i<n;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      var swat=this.innerHTML;
+     makesound(swat);
+     buttonanimation(swat);
+    });
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonanimation(event.key);
+});
+function makesound(key){
+   switch(key){
+        case "w":
+        var tone1=new Audio('sounds/tom-1.mp3');
+        tone1.play();
+        break;
+        case "a":
+        var tone2=new Audio('sounds/tom-2.mp3');
+        tone2.play();
+        break;
+        case "s":
+        var tone3=new Audio('sounds/tom-3.mp3');
+        tone3.play();
+        break;
+        case "d":
+        var tone4=new Audio('sounds/tom-4.mp3');
+        tone4.play();
+        break;
+        case "j":
+        var tone5=new Audio('sounds/snare.mp3');
+        tone5.play();
+        break;
+        case "k":
+        var tone6=new Audio('sounds/crash.mp3');
+        tone6.play();
+        break;
+        case "l":
+        var tone7=new Audio('sounds/kick-bass.mp3');
+        tone7.play();
+        break;
+        default:console.log(swat);
+      }
+};
+function buttonanimation(key){
+  var activebutton=document.querySelector("."+key);
+  activebutton.classList.add("pressed");
+  setTimeout(function(){
+    activebutton.classList.remove("pressed");
+  },100);
+};
